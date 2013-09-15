a5fba9ac4b8f6481c3563b8600fb8364eb99819c
Stefan Agner <stefan@agner.ch>
2013-09-15 15:21:58 +0200

    ar71xx: create new subtarget nand-yaffs2
    This target will be used for Mikrotik devices using YAFFS2 flash.
    The subtarget nand will support NAND devices on UBI/UBIFS.

---
+++ target/linux/ar71xx/nand-yaffs2/target.mk
@@ -0,0 +1,8 @@
+BOARDNAME:=Devices with NAND flash using YAFFS2
+
+define Target/Description
+	Build firmware images for Atheros AR71xx/AR913x based boards with 
+	NAND flash and YAFFS2 filesystem, e.g. MikroTik RB-4xx or RB-750
+endef
+
+
